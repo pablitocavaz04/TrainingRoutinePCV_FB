db9b06d63b9f0e38bdc2e749109d63c5d2eece95
pablitocavaz04 <pablocavaz2004@gmail.com>
2025-02-05 12:52:17 +0100

    Animations

--- src/app/components/auth-modal/auth-modal.component.ts
+++ src/app/components/auth-modal/auth-modal.component.ts
@@ -7,7 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   standalone:false
 })
 export class AuthModalComponent {
-  @Input() isLoginMode: boolean = true; // Se recibe desde la Landing Page
+  @Input() isLoginMode: boolean = true; // Este estado lo recibimos desde la landing page.
   @Output() close = new EventEmitter<void>();
 
   closeModal() {
