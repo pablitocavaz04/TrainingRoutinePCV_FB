3f157f7745dd78ec10cc0790cc92dc251d85b712
pablito04 <pablocavaz2004@gmail.com>
2025-02-13 00:17:28 +0100

    Final Nav Bar and Modal profile End with Animations, just logic

--- src/app/components/profile-modal/profile-modal.component.ts
+++ src/app/components/profile-modal/profile-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-profile-modal',
@@ -6,14 +6,31 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./profile-modal.component.scss'],
   standalone: false
 })
-export class ProfileModalComponent {
+export class ProfileModalComponent implements OnChanges {
   @Input() isOpen: boolean = false;
   @Input() userName: string = "Nombre de Usuario";
   @Input() userEmail: string = "usuario@email.com";
   @Input() userImage: string = "https://via.placeholder.com/100";
   @Output() close = new EventEmitter<void>();
 
+  modalState: 'open' | 'closed' = 'closed';
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes["isOpen"]) {
+      if (this.isOpen) {
+        setTimeout(() => {
+          this.modalState = 'open';
+        }, 10);
+      } else {
+        this.modalState = 'closed';
+      }
+    }
+  }
+
   closeModal() {
-    this.close.emit();
+    this.modalState = 'closed';
+    setTimeout(() => {
+      this.close.emit();
+    }, 300);
   }
 }
